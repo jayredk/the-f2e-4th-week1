67f46fffd870a72b815cc28214497c4a0d5fdd67
Jay <kred110396@gmail.com>
2023-02-21 23:21:17 +0800

    feat: Finished block 6

--- src/App.vue
+++ src/App.vue
@@ -527,6 +527,63 @@ import AppHeader from "@/components/AppHeader.vue";
         </div>
       </div>
     </section>
+
+    <section id="bk6-1" class="container relative text-center">
+      <h2 class="text-highlight">
+        區區修煉<br class="lg:hidden" />已經無法滿足了嗎？
+      </h2>
+      <img
+        class="absolute top-[200%] left-0 hidden w-[368px] lg:block"
+        src="./assets/images/bg/bg_decorate_03.png"
+        alt="bg_decorate_03"
+      />
+      <img
+        class="absolute top-[150%] right-0 hidden w-[450px] lg:block"
+        src="./assets/images/bg/bg_decorate_07.png"
+        alt="bg_decorate_07"
+      />
+    </section>
+
+    <section id="bk6-2" class="mt-[100%] mb-16">
+      <h3
+        class="mb-3 w-full bg-[url('@/assets/images/bg/bg_talking_c.png')] bg-[length:100%_100%] bg-bottom bg-no-repeat pb-6 pt-3 text-center text-2xl font-bold text-primary lg:mb-16 lg:bg-[url('@/assets/images/bg/bg_talking.png')] lg:bg-[length:627px_151px] lg:px-16 lg:pt-6 lg:pb-16 lg:text-6xl"
+      >
+        還有比賽等著你！
+      </h3>
+
+      <div class="container lg:flex lg:justify-between">
+        <img
+          class="mx-auto w-[335px] lg:m-0 lg:w-[375px]"
+          src="./assets/images/main/award.png"
+          alt="award"
+        />
+        <div>
+          <h4 class="mb-6 text-highlight">評審機制</h4>
+          <p class="text-primary">
+            初選： 將由六角學院前端、UI 評審進行第一波篩選。
+          </p>
+          <p class="mb-6 text-primary">
+            決選： 由六角學院與贊助廠商討論，進行最後篩選，並於 12/30(五)
+            由評審進行直播公布名單！
+          </p>
+          <h4 class="mb-6 text-highlight">獎項</h4>
+          <ul class="list-decimal pl-5 text-primary">
+            <li>初選佳作 共六十位 數位獎狀</li>
+            <li>
+              個人企業獎 共六位 NTD
+              <h5 class="inline-block text-highlight">3,000</h5>
+              /位
+            </li>
+            <li>
+              團體企業獎 共三組 NTD
+              <h5 class="inline-block text-highlight">10,000</h5>
+              /組
+            </li>
+            <li>以上皆提供完賽數位獎狀</li>
+          </ul>
+        </div>
+      </div>
+    </section>
     <!-- <div
       id="character"
       class="fixed bottom-0 z-20 flex max-w-[inherit] items-center justify-center"
